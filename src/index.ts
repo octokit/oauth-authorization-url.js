15a8d0342f31033eef7e1c56a3cc999915b82f68
wolfy1339 <4595477+wolfy1339@users.noreply.github.com>
2019-04-21 11:13:10 -0400

    fix: include all parameters in url (#10)
    Make a URL builder to include all parameters in the final returned URL when the function gets called

--- src/index.ts
+++ src/index.ts
@@ -28,13 +28,38 @@ export function oauthLoginUrl (options: Options): Result {
     ? options.scopes.split(/[,\s]+/).filter(Boolean)
     : Array.isArray(options.scopes) ? options.scopes : []
 
-  return {
+  const result: Result = {
     allowSignup: options.allowSignup === false ? false : true,
     clientId: options.clientId,
     login: options.login || null,
     redirectUrl: options.redirectUrl || null,
     scopes: scopesNormalized,
     state: options.state || Math.random().toString(36).substr(2),
-    url: `${BASE_URL}?client_id=${options.clientId}`
+    url: ''
   }
+  result.url = urlBuilderAuthorize(BASE_URL, result)
+
+  return result
+}
+type ResultKeys = Exclude<keyof Result, 'url'>
+function urlBuilderAuthorize (base: string, options: Result) {
+  const map = {
+    allowSignup: 'allow_signup',
+    clientId: 'client_id',
+    login: 'login',
+    redirectUrl: 'redirect_url',
+    scopes: 'scopes',
+    state: 'state',
+  }
+
+  let url = base
+
+  Object.entries(options).filter(([k, v]) => v !== null && k !== 'url') // Filter out keys that are null and remove the url key
+    .filter(([, v]) => Array.isArray(v) ? v.length !== 0 : true) // Filter out empty Array
+    .map(([key, ]) => [ map[key as ResultKeys], `${options[key as ResultKeys]!}` ]) // Map Array with the proper URL parameter names and change the value to a string using template strings
+    .forEach(([key, value], index) => { // Finally, build the URL
+      url += index === 0 ? `?` : '&'
+      url += `${key}=${value}`
+    })
+  return url
 }
