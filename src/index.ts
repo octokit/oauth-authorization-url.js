3fecb8996dd29dced8f7dcf8a1ee21d737ecfb35
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-03-14 15:02:43 -0700

    feat: `clientType` option and different results for GitHub Apps and OAuth Apps (#129)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Options, Result, ResultKeys } from "./types";
+import { ClientType, Options, Result } from "./types";
 
-export function oauthAuthorizationUrl(options: Options): Result {
+export function oauthAuthorizationUrl<
+  TClientType extends ClientType = "oauth-app"
+>(options: Options<TClientType>): Result<TClientType> {
   const scopesNormalized =
     typeof options.scopes === "string"
       ? options.scopes.split(/[,\s]+/).filter(Boolean)
@@ -8,23 +10,38 @@ export function oauthAuthorizationUrl(options: Options): Result {
       ? options.scopes
       : [];
 
-  const result: Result = {
+  const clientType = options.clientType || "oauth-app";
+  const baseUrl = options.baseUrl || "https://github.com";
+  const common = {
     allowSignup: options.allowSignup === false ? false : true,
     clientId: options.clientId,
     login: options.login || null,
     redirectUrl: options.redirectUrl || null,
-    scopes: scopesNormalized,
     state: options.state || Math.random().toString(36).substr(2),
     url: "",
   };
 
-  const baseUrl = options.baseUrl || "https://github.com";
+  const result =
+    clientType === "oauth-app"
+      ? {
+          ...common,
+          clientType: "oauth-app",
+          scopes: scopesNormalized,
+        }
+      : {
+          ...common,
+          clientType: "github-app",
+        };
+
   result.url = urlBuilderAuthorize(`${baseUrl}/login/oauth/authorize`, result);
 
-  return result;
+  return result as Result<TClientType>;
 }
 
-function urlBuilderAuthorize(base: string, options: Result) {
+function urlBuilderAuthorize(
+  base: string,
+  options: Record<string, unknown>
+): string {
   const map = {
     allowSignup: "allow_signup",
     clientId: "client_id",
@@ -36,14 +53,21 @@ function urlBuilderAuthorize(base: string, options: Result) {
 
   let url = base;
 
-  Object.entries(options)
-    .filter(([k, v]) => v !== null && k !== "url") // Filter out keys that are null and remove the url key
-    .filter(([, v]) => (Array.isArray(v) ? v.length !== 0 : true)) // Filter out empty Array
-    .map(([key]) => [map[key as ResultKeys], `${options[key as ResultKeys]!}`]) // Map Array with the proper URL parameter names and change the value to a string using template strings
+  Object.keys(map)
+    .filter((k) => options[k] !== null) // Filter out keys that are null and remove the url key
+    .filter((k) => {
+      if (k !== "scopes") return true;
+      if (options.clientType === "github-app") return false;
+
+      return !Array.isArray(options[k]) || (options[k] as string[]).length > 1;
+    }) // Filter out empty scopes array
+    // @ts-ignore
+    .map((key) => [map[key], `${options[key]}`]) // Map Array with the proper URL parameter names and change the value to a string using template strings
     .forEach(([key, value], index) => {
       // Finally, build the URL
       url += index === 0 ? `?` : "&";
       url += `${key}=${value}`;
     });
+
   return url;
 }
--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,62 @@
-export interface Options {
+export type ClientType = "oauth-app" | "github-app";
+
+export type OAuthAppOptions<TClientType extends "oauth-app"> = {
   clientId: string;
 
+  clientType?: TClientType;
   allowSignup?: boolean;
   login?: string;
   scopes?: string | string[];
   redirectUrl?: string;
   state?: string;
   baseUrl?: string;
-}
-export interface Result {
+};
+
+export type GitHubAppOptions<TClientType extends "github-app"> = {
+  clientId: string;
+
+  clientType: TClientType;
+  /** `scopes` are not permitted for GitHub Apps */
+  scopes?: never;
+  allowSignup?: boolean;
+  login?: string;
+  redirectUrl?: string;
+  state?: string;
+  baseUrl?: string;
+};
+
+export type Options<
+  TClientType extends ClientType
+> = TClientType extends "oauth-app"
+  ? OAuthAppOptions<TClientType>
+  : TClientType extends "github-app"
+  ? GitHubAppOptions<TClientType>
+  : never;
+
+type OAuthAppResult<TClientType extends "oauth-app"> = {
   allowSignup: boolean;
   clientId: string;
+  clientType: TClientType;
   login: string | null;
   redirectUrl: string | null;
   scopes: string[];
   state: string;
   url: string;
-}
+};
+type GitHubAppResult<TClientType extends "github-app"> = {
+  allowSignup: boolean;
+  clientId: string;
+  clientType: TClientType;
+  login: string | null;
+  redirectUrl: string | null;
+  state: string;
+  url: string;
+};
 
-export type ResultKeys = Exclude<keyof Result, "url">;
+export type Result<
+  TClientType extends ClientType
+> = TClientType extends "oauth-app"
+  ? OAuthAppResult<TClientType>
+  : TClientType extends "github-app"
+  ? GitHubAppResult<TClientType>
+  : never;
